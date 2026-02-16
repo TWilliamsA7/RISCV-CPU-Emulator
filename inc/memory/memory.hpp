18ebd0ca0b3206f576fbe289a1db46b9c3b8b62b
BatsCrow <twilliamsa776@gmail.com>
2026-02-15 19:06:25 -0500

    Defined memory interface

--- inc/memory/memory.hpp
+++ inc/memory/memory.hpp
@@ -3,13 +3,28 @@
 #pragma once
 #include <cstdint>
 
+#include <vector>
+#include <cstdint>
+
 class Memory {
     public:
-        uint8_t read8(uint32_t addr);
-        uint16_t read16(uint32_t addr);
-        uint32_t read32(uint32_t addr);
+        explicit Memory(size_t size);
+
+        uint8_t  read8(uint32_t addr) const;
+        uint16_t read16(uint32_t addr) const;
+        uint32_t read32(uint32_t addr) const;
 
         void write8(uint32_t addr, uint8_t val);
         void write16(uint32_t addr, uint16_t val);
         void write32(uint32_t addr, uint32_t val);
+
+        void loadBinary(const std::vector<uint8_t>& program, uint32_t startAddr);
+        void reset();
+
+        size_t size() const;
+
+    private:
+        std::vector<uint8_t> data_;
+        void checkAddress(uint32_t addr, size_t width) const;
+
 };
--- src/memory/memory.cpp
+++ src/memory/memory.cpp
@@ -1,9 +1,62 @@
 // src/memory/memory.cpp
 
-namespace memory {
+#include "memory/memory.hpp"
+#include <stdexcept>
 
-    int memory_sanity() {
-        return 0;
-    }
+Memory::Memory(size_t size) : data_(size, 0) {}
 
+uint8_t Memory::read8(uint32_t addr) const {
+    checkAddress(addr, 1);
+    return data_[addr];
 }
+
+uint16_t Memory::read16(uint32_t addr) const {
+    checkAddress(addr, 2);
+    return data_[addr] |
+           (data_[addr + 1] << 8);
+}
+
+uint32_t Memory::read32(uint32_t addr) const {
+    checkAddress(addr, 4);
+    return data_[addr] |
+           (data_[addr + 1] << 8) |
+           (data_[addr + 2] << 16) |
+           (data_[addr + 3] << 24);
+}
+
+void Memory::write8(uint32_t addr, uint8_t val) {
+    checkAddress(addr, 1);
+    data_[addr] = val;
+}
+
+void Memory::write16(uint32_t addr, uint16_t val) {
+    checkAddress(addr, 2);
+    data_[addr] = val & 0xFF;
+    data_[addr] = (val >> 8) & 0xFF;
+}
+
+void Memory::write32(uint32_t addr, uint32_t val) {
+    checkAddress(addr, 4);
+    data_[addr] = val & 0xFF;
+    data_[addr] = (val >> 8) & 0xFF;
+    data_[addr] = (val >> 16) & 0xFF;
+    data_[addr] = (val >> 24) & 0xFF;
+}
+
+size_t Memory::size() const { return data_.size(); }
+
+void Memory::loadBinary(const std::vector<uint8_t>& program, uint32_t startAddr) {
+    checkAddress(startAddr, program.size());
+    std::copy(program.begin(), program.end(), data_.begin() + startAddr);
+}
+
+void Memory::reset() {
+    std::fill(data_.begin(), data_.end(), 0);
+}
+
+void Memory::checkAddress(uint32_t addr, size_t width) const {
+    if (addr + width > data_.size()) {
+        throw std::out_of_range("Memory access out of bounds");
+    }
+}
+
