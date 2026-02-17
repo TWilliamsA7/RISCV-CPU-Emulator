6a3ae0ccba3eda6f82ae4b341a80dbcc17d7b688
BatsCrow <twilliamsa776@gmail.com>
2026-02-16 22:39:38 -0500

    Capture value change

--- inc/core/state.hpp
+++ inc/core/state.hpp
@@ -2,10 +2,29 @@
 
 #pragma once
 
-namespace core {
+#include "isa/isa.hpp"
+#include <cstdint>
+#include <optional>
 
-    struct CpuState {
-        int dummy = 0;
-    };
+struct RegWrite {
+    uint8_t rd;
+    uint32_t old_val;
+    uint32_t new_val;
+};
 
-}
+struct MemWrite {
+    uint32_t addr;
+    uint32_t old_val;
+    uint32_t new_val;
+    uint8_t size; // 1, 2, or 4 bytes
+};
+
+struct StepResult {
+    uint32_t pc_before;
+    uint32_t instruction;
+    DecodedInstr dInstr;
+    uint32_t pc_after;
+
+    std::optional<RegWrite> reg_write;
+    std::optional<MemWrite> mem_write;
+};
--- src/cpu/execute.cpp
+++ src/cpu/execute.cpp
@@ -50,188 +50,242 @@ void CPU::execute(const DecodedInstr& i) {
 }
 
 void CPU::execADD(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     int32_t b = static_cast<int32_t>(r[i.rs2]);
     int32_t result = a + b;
     writeReg(i.rd, static_cast<uint32_t>(result));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execSUB(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     int32_t b = static_cast<int32_t>(r[i.rs2]);
     int32_t result = a - b;
     writeReg(i.rd, static_cast<uint32_t>(result));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execAND(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     int32_t b = static_cast<int32_t>(r[i.rs2]);
     int32_t result = a & b;
     writeReg(i.rd, static_cast<uint32_t>(result));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execOR(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     int32_t b = static_cast<int32_t>(r[i.rs2]);
     int32_t result = a | b;
     writeReg(i.rd, static_cast<uint32_t>(result));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execXOR(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     int32_t b = static_cast<int32_t>(r[i.rs2]);
     int32_t result = a ^ b;
     writeReg(i.rd, static_cast<uint32_t>(result));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execSLL(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     int32_t b = static_cast<int32_t>(r[i.rs2]);
     int32_t result = a << b;
     writeReg(i.rd, static_cast<uint32_t>(result));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execSRL(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     uint32_t a = r[i.rs1];
     uint32_t b = r[i.rs2];
     uint32_t result = a >> b;
     writeReg(i.rd, static_cast<uint32_t>(result));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execSRA(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     int32_t b = static_cast<int32_t>(r[i.rs2]);
     int32_t result = a >> b;
     writeReg(i.rd, static_cast<uint32_t>(result));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execSLT(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     int32_t b = static_cast<int32_t>(r[i.rs2]);
     writeReg(i.rd, static_cast<uint32_t>(a < b ? 1 : 0));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execSLTU(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     uint32_t a = r[i.rs1];
     uint32_t b = r[i.rs2];
     writeReg(i.rd, static_cast<uint32_t>(a < b ? 1 : 0));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execADDI(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     writeReg(i.rd, static_cast<uint32_t>(a + i.imm));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execANDI(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     writeReg(i.rd, static_cast<uint32_t>(a & i.imm));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execORI(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     writeReg(i.rd, static_cast<uint32_t>(a | i.imm));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execXORI(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     writeReg(i.rd, static_cast<uint32_t>(a ^ i.imm));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execSLTI(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     writeReg(i.rd, static_cast<uint32_t>(a < i.imm ? 1 : 0));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execSLTIU(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     uint32_t a = r[i.rs1];
     writeReg(i.rd, static_cast<uint32_t>(a < i.imm ? 1 : 0));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execSLLI(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     writeReg(i.rd, static_cast<uint32_t>(a << i.imm));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execSRLI(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     uint32_t a = r[i.rs1];
     writeReg(i.rd, static_cast<uint32_t>(a >> i.imm));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execSRAI(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     int32_t a = static_cast<int32_t>(r[i.rs1]);
     writeReg(i.rd, static_cast<uint32_t>(a >> i.imm));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execLW(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     uint32_t a = r[i.rs1] + i.imm;
     writeReg(i.rd, memory_.read32(a));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execLH(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     uint32_t a = r[i.rs1]  + i.imm;
     int16_t half = memory_.read16(a);       
     int32_t s = static_cast<int32_t>(half);
     writeReg(i.rd, static_cast<uint32_t>(s));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execLHU(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     uint32_t a = r[i.rs1]  + i.imm;
     uint32_t u = static_cast<uint32_t>(memory_.read16(a));
     writeReg(i.rd, u);
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execLB(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     uint32_t a = r[i.rs1]  + i.imm;
     int8_t byte = memory_.read8(a);       
     int32_t s = static_cast<int32_t>(byte);
     writeReg(i.rd, static_cast<uint32_t>(s));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execLBU(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     uint32_t a = r[i.rs1] + i.imm;
     uint32_t u = static_cast<uint32_t>(memory_.read8(a));
     writeReg(i.rd, u);
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd]};
     pc_ += 4;
 }
 
 void CPU::execSW(const DecodedInstr& i) {
     uint32_t a = r[i.rs1] + i.imm;
+    uint32_t o = memory_.read32(a);
     memory_.write32(a, r[i.rs2]);
+    sr.mem_write = MemWrite{ a, o, r[i.rs2], 4};
     pc_ += 4;
 }
 
 void CPU::execSH(const DecodedInstr& i) {
     uint32_t a = r[i.rs1] + i.imm;
+    uint32_t o = static_cast<uint32_t>(memory_.read16(a));
     memory_.write16(a, static_cast<uint16_t>(r[i.rs2]));
+    sr.mem_write = MemWrite{ a, o, r[i.rs2], 2};
     pc_ += 4;
 }
 
 void CPU::execSB(const DecodedInstr& i) {
     uint32_t a = r[i.rs1] + i.imm;
+    uint32_t o = static_cast<uint32_t>(memory_.read8(a));
     memory_.write8(a, static_cast<uint8_t>(r[i.rs2]));
+    sr.mem_write = MemWrite{ a, o, r[i.rs2], 1};
     pc_ += 4;
 }
 
@@ -282,22 +336,30 @@ void CPU::execBGEU(const DecodedInstr& i) {
 }
 
 void CPU::execJAL(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     writeReg(i.rd, pc_ + 4);
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd] };
     pc_ += i.imm;
 }
 
 void CPU::execJALR(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     writeReg(i.rd, pc_ + 4);
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd] };
     pc_ = (r[i.rs1] + i.imm) &~ 1;
 }
 
 void CPU::execLUI(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     writeReg(i.rd, static_cast<uint32_t>(i.imm));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd] };
     pc_ += 4;
 }
 
 void CPU::execAUIPC(const DecodedInstr& i) {
+    uint32_t o = r[i.rd];
     writeReg(i.rd, static_cast<uint32_t>(pc_ + i.imm));
+    sr.reg_write = RegWrite{ i.rd, o, r[i.rd] };
     pc_ += 4;
 }
 
