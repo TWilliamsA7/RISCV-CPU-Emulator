25bba317430a8de9d28abff94c2924142a5d1cff
BatsCrow <twilliamsa776@gmail.com>
2026-03-21 01:09:06 -0400

    Distinct constrained instruction generation utilizing state

--- tools/config/context.py
+++ tools/config/context.py
@@ -5,3 +5,6 @@ class TestContext:
         self.config = config
         self.coverage = coverage
         self.state = GenState(config)
+
+    def update_state(self, instr, rd, rs1, rs2, imm):
+        self.state.update_state(instr, rd, rs1, rs2, imm)
--- tools/testgen/generators/program.py
+++ tools/testgen/generators/program.py
@@ -1,31 +1,65 @@
 from testgen.generators.instruction import choose_instruction
 from testgen.constraints import *
 from testgen.encode import encode
+from testgen.isa import INSTRUCTIONS, Instr, INSTRUCTIONS_SIZE_MAP
 
-def generate_program(instrs, coverage, length=100):
+def generate_program(ctx):
     program = []
 
-    for _ in range(length):
-        instr = choose_instruction(instrs, coverage)
-
-        rd = gen_reg()
-        rs1 = gen_reg()
-        rs2 = gen_reg()
-
-        imm = None
-
-        match instr.value[0]:
-            case 0x23 | 0x13 | 0x03 | 0x67 if instr.name not in ["SLLI", "SRLI", "SRAI"]:
-                imm = gen_imm_i()
-            case 0x13:
-                imm = gen_shift_imm()
-            case 0x63:
-                imm = gen_branch_offset()
-            case 0x37 | 0x17:
-                imm = gen_imm_u()
-            case 0x6F:
-                imm = gen_jump_offset()
-
-        program.append(encode(instr, rd=rd, rs1=rs1, rs2=rs2, imm=imm))
+    for _ in range(ctx.config.program_length):
+        instr = choose_instruction(INSTRUCTIONS, ctx.coverage)
+        entry = generate_instruction(ctx, instr)
+        program.append(entry)
 
     return program
+
+def generate_instruction(ctx, instr):
+    state = ctx.state
+
+    rd = gen_reg()
+    rs1 = gen_reg()
+    rs2 = gen_reg()
+
+    imm = None
+
+    match instr.value[0]:
+        case 0x03:  # LOAD
+            try: 
+                access_size = INSTRUCTIONS_SIZE_MAP[instr.name]
+                rs1, imm = gen_valid_address(ctx, access_size)
+            except RuntimeError:
+                instr = Instr.INVALID
+
+        case 0x23:  # STORE
+            try: 
+                access_size = INSTRUCTIONS_SIZE_MAP[instr.name]
+                rs1, imm = gen_valid_address(ctx, access_size)
+            except RuntimeError:
+                instr = Instr.INVALID
+
+        case 0x63:  # BRANCH
+            try:
+                rs1, rs2 = gen_branch(ctx)
+            except RuntimeError:
+                instr = Instr.INVALID
+
+        case 0x6F:  # JAL
+            try:
+                rs1, imm = gen_jalr(ctx)
+            except RuntimeError:
+                instr = Instr.INVALID
+
+        case 0x13 if instr.name in ["SLLI", "SRLI", "SRAI"]:
+            imm = gen_shift_imm()
+
+        case 0x13 | 0x67:
+            imm = gen_imm_i()
+
+        case 0x37 | 0x17:
+            imm = gen_imm_u()
+
+    encoded = encode(instr, rd=rd, rs1=rs1, rs2=rs2, imm=imm)
+
+    ctx.update_state(instr, rd, rs1, rs2, imm)
+
+    return encoded
