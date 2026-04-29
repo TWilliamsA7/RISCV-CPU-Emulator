07c304119fa310fb59fe487e87b3c98e32bc1619
BatsCrow <twilliamsa776@gmail.com>
2026-04-29 19:48:17 -0400

    More complicated test program

--- tools/test_programs/test.c
+++ tools/test_programs/test.c
@@ -1,7 +1,14 @@
 // test.c
 
 int main() {
-    int a = 6;
-    int b = 7;
-    return a * b + 9;
+    int a = 12;
+    int b = 14;
+
+    int c = a * b;
+
+    for (int i = 0; i < 3; i++) {
+        c /= 2;
+    }
+
+    return c + (b % a);
 }
