42ca5eedf1d92e558dc735d47dd8ab26877671ed
BatsCrow <twilliamsa776@gmail.com>
2026-04-29 15:34:09 -0400

    Cleared previous main function

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,5 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cout << "Usage: ./emulator {program.bin}\n";
-        return 1;
-    }
-
-    Memory mem{4096};
-    CPU cpu{mem, true};
-
-    mem.loadBinary(argv[1], 0);
-
-    cpu.run(1024);
     return 0;
 }
