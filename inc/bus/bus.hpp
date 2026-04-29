58f118103a9904115cba9ffb481504810bcb2105
BatsCrow <twilliamsa776@gmail.com>
2026-04-29 14:30:44 -0400

    Initial bus and dram definition

---
+++ inc/bus/bus.hpp
@@ -0,0 +1,38 @@
+// inc/bus/bus.hpp
+
+#include <cstdint>
+#include <vector>
+
+// Interface for reading writing to perhiperals
+class Bus {
+    public:
+
+        // Starting point of DRAM addresses
+        static constexpr uint32_t DRAM_BASE = 0x80000000;
+
+        // Size of avaiable DRAM
+        static constexpr uint32_t DRAM_SIZE = 1024 * 1024 * 128;
+
+        // Read 32 bit value located at addr 
+        uint32_t read32(uint32_t addr) const;
+
+        // Read 16 bit value located at addr 
+        uint16_t read16(uint32_t addr) const;
+
+        // Read 8 bit value located at addr
+        uint8_t read8(uint32_t addr) const;
+
+        // Write 32 bit value to addr
+        void write32(uint32_t addr, uint32_t val);
+
+        // Write 16 bit value to addr
+        void write16(uint32_t addr, uint16_t val);
+
+        // Write 32 bit value to addr
+        void write8(uint32_t addr, uint8_t val);
+
+    private:
+
+        // Dynamic Random Access Memory
+        std::vector<uint8_t> dram_;
+};
---
+++ src/bus/bus.cpp
@@ -0,0 +1,52 @@
+// src/bus/bus.cpp
+
+#include "bus/bus.hpp"
+
+uint8_t Bus::read8(uint32_t addr) const {
+    if (addr >= Bus::DRAM_BASE && addr < DRAM_BASE + DRAM_SIZE) {
+        uint32_t offset = addr - Bus::DRAM_BASE;
+        return dram_[offset];
+    }
+    return 0;
+}
+
+uint16_t Bus::read16(uint32_t addr) const {
+    if (addr >= Bus::DRAM_BASE && addr < DRAM_BASE + DRAM_SIZE) {
+        uint32_t offset = addr - Bus::DRAM_BASE;
+        return dram_[offset] | (dram_[offset+1] << 8);
+    }
+    return 0;
+}
+
+uint32_t Bus::read32(uint32_t addr) const {
+    if (addr >= Bus::DRAM_BASE && addr < DRAM_BASE + DRAM_SIZE) {
+        uint32_t offset = addr - Bus::DRAM_BASE;
+        return dram_[offset] | (dram_[offset+1] << 8) | (dram_[offset+2] << 16) | (dram_[offset+3] << 24);
+    }
+    return 0;
+}
+
+void Bus::write8(uint32_t addr, uint8_t val) {
+    if (addr >= Bus::DRAM_BASE && addr < DRAM_BASE + DRAM_SIZE) {
+        uint32_t offset = addr - Bus::DRAM_BASE;
+        dram_[offset] = val;
+    }
+}
+
+void Bus::write16(uint32_t addr, uint16_t val) {
+    if (addr >= Bus::DRAM_BASE && addr < DRAM_BASE + DRAM_SIZE) {
+        uint32_t offset = addr - Bus::DRAM_BASE;
+        dram_[offset] = val & 0xFF;
+        dram_[offset + 1] = (val >> 8) & 0xFF;
+    }
+}
+
+void Bus::write32(uint32_t addr, uint32_t val) {
+    if (addr >= Bus::DRAM_BASE && addr < DRAM_BASE + DRAM_SIZE) {
+        uint32_t offset = addr - Bus::DRAM_BASE;
+        dram_[offset] = val & 0xFF;
+        dram_[offset + 1] = (val >> 8) & 0xFF;
+        dram_[offset + 2] = (val >> 16) & 0xFF;
+        dram_[offset + 3] = (val >> 24) & 0xFF;
+    }
+}
