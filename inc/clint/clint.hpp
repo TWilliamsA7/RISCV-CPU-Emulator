12e975786a52dac0a7ce4c49902d614d5168b862
BatsCrow <twilliamsa776@gmail.com>
2026-05-03 18:05:11 -0400

    Fixed nibble alignment in clint

--- inc/clint/clint.hpp
+++ inc/clint/clint.hpp
@@ -10,13 +10,13 @@ class Clint {
 
         Clint();
 
-        static constexpr uint32_t BASE        = 0x2000000;
-        static constexpr uint32_t MSIP_OFFSET = 0x0000000;
-        static constexpr uint32_t MTIMECMP_LO = 0x0004000;
-        static constexpr uint32_t MTIMECMP_HI = 0x0004004;
-        static constexpr uint32_t MTIME_LO    = 0x000BFF8;
-        static constexpr uint32_t MTIME_HI    = 0x000BFFC;
-        static constexpr uint32_t SIZE        = 0x000C000;
+        static constexpr uint32_t BASE        = 0x02000000;
+        static constexpr uint32_t MSIP_OFFSET = 0x00000000;
+        static constexpr uint32_t MTIMECMP_LO = 0x00004000;
+        static constexpr uint32_t MTIMECMP_HI = 0x00004004;
+        static constexpr uint32_t MTIME_LO    = 0x0000BFF8;
+        static constexpr uint32_t MTIME_HI    = 0x0000BFFC;
+        static constexpr uint32_t SIZE        = 0x0000C000;
 
         bool msip = false;           // software interrupt pending
         uint64_t mtime    = 0;
@@ -28,7 +28,7 @@ class Clint {
 
     private:
         std::chrono::high_resolution_clock::time_point start_time_;
-        uint64_t frequency_ = 10e6;
+        uint64_t frequency_ = 10000000;
 
 
 };
