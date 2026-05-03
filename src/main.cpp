b360c12ed17cb539a6f44e5dce3db58a106ea75d
BatsCrow <twilliamsa776@gmail.com>
2026-05-03 00:03:19 -0400

    Added basic CLI implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,35 @@
 #include "cpu/cpu.hpp"
 #include "elf/elf.hpp"
 #include <iostream>
+#include <unistd.h>
 
 int main(int argc, char** argv) {
 
-    if (argc < 2) {
-        std::cout << "Usage: ./emulator {program.elf}\n";
+    CPUConfig config;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "mc")) != -1) {
+        switch (opt) {
+            case 'm': config.extension_m = true; break;
+            case 'c': config.extension_c = true; break;
+            default:
+                std::cerr << "Usage: " << argv[0] << " [-m] [-c] <elf_file>" << std::endl;
+                return 1;
+        }
+    }
+
+    if (optind >= argc) {
+        std::cerr << "Expected ELF file after options" << std::endl;
         return 1;
     }
 
+    char* elf_path = argv[optind];
+
     Clint clint;
     Bus bus = Bus(clint);
-    CPU cpu(bus, clint);
+    CPU cpu(config, bus, clint);
 
-    load_elf(argv[1], bus, cpu);
+    load_elf(elf_path, bus, cpu);
 
     cpu.run();
 
