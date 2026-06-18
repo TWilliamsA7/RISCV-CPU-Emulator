0868b3e6a1b0f8c59d888d0dbffc308796b9cbaf
BatsCrow <twilliamsa776@gmail.com>
2026-06-18 16:17:02 -0700

    Updated emulator initialization

--- inc/emulator.hpp
+++ inc/emulator.hpp
@@ -22,4 +22,6 @@ class Emulator {
         MMU mmu;
 
         EmulatorConfig config_;
+
+        int emulate();
 };
--- src/emulator.cpp
+++ src/emulator.cpp
@@ -3,12 +3,35 @@
 #include "emulator.hpp"
 
 #include <iostream>
+#include "elf/elf.hpp"
 
 Emulator::Emulator(EmulatorConfig config) : 
     config_(config), 
     clint(*this), plic(*this), 
     bus(*this), cpu(*this), mmu(*this) {
 
-    std::cout << "Initializing Emulator...\n";
+    if (config_.elf_path == nullptr) {
+        throw std::runtime_error("[ERROR] Missing ELF path...\n");
+    }
 
+    if (config_.cpu_config.mode == ExecutionMode::SYSTEM && config_.disk_path == nullptr) {
+        throw std::runtime_error("[ERROR] Missing disk path for system level execution...\n");
+    }
+
+    std::cout << "[INFO] Initializing Emulator...\n";
+
+    load_elf(config_.elf_path, bus, cpu);
+
+    std::cout << "[INFO] Emulator Ready\n";
+}
+
+int Emulator::emulate() {
+    try {
+        std::cout << "[INFO] Starting Emulator\n";
+        cpu.run();
+    } catch (const ProgramExit& e) {
+        return e.code;
+    }
+
+    return 0;
 }
